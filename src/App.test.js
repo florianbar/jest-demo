e4066fab57a9b781af00836d1bde59cc6f24cd5c
Florian <florianbar01@gmail.com>
2020-11-11 09:31:35 +0200

    Import shallow from enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,10 @@
-import Enzyme from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test('renders learn react link', () => {
-  
+test('renders without crashing', () => {
+    const wrapper = shallow(<App />);
 });
